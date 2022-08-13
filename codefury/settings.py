a9f994e5a5d67768c15422bb44f91c36b746c842
sinpaiiii <mohammednowfal7228@gmail.com>
2022-08-14 03:40:43 +0530

    first

--- codefury/settings.py
+++ codefury/settings.py
@@ -127,4 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #activate herolu
-django_heroku.seetings(locals())
+django_heroku.settings(locals())
